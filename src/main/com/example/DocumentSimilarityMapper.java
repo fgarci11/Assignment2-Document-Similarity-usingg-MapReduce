5f415a87b6e72e8f7d07cdd545c3065750b0c3ab
fgarci11 <fgarci11@charlotte.edu>
2025-09-24 05:54:51 +0000

    feat(driver/mapper/reducer)!: Working on driver, mapper, and reducer

--- src/main/com/example/DocumentSimilarityMapper.java
+++ src/main/com/example/DocumentSimilarityMapper.java
@@ -1,5 +1,33 @@
 package com.example;
 
-public class DocumentSimilarityMapper  {
-    
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class DocumentSimilarityMapper extends Mapper<Text, Text, Text, Text> {
+
+    private Text word = new Text();
+    private Text docId = new Text();
+
+    @Override
+    protected void map(Text key, Text value, Context context) throws IOException, InterruptedException {
+        // Split document ID from document
+        String[] parts = value.toString().split("\\s+", 2);
+        if (parts.length < 2){
+            throw new IOException("Invalid input format: expected at least two parts, found " + parts.length);
+            // return
+        }
+
+        String documentId = parts[0];
+        String documentContent = parts[1].toLowerCase().replaceAll("[^a-z0-9\\s]", ""); // Lowercase + strip punctuation
+        docId.set(documentId); 
+
+        StringTokenizer itr = new StringTokenizer(documentContent);
+        while (itr.hasMoreTokens()) {
+            word.set(itr.nextToken());
+            context.write(word, docId);
+        }
+    }
 }
--- src/main/com/example/controller/DocumentSimilarityDriver.java
+++ src/main/com/example/controller/DocumentSimilarityDriver.java
@@ -1,8 +1,39 @@
 package com.example.controller;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import com.example.DocumentSimilarityMapper;
+import com.example.DocumentSimilarityReducer;
+
 public class DocumentSimilarityDriver {
     
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws Exception{
+        if (args.length != 2) {
+            System.err.println("Usage: DocumentSimilarityDriver <input path> <output path>");
+            System.exit(-1);
+        }
+
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "jaccard similarity");
+
+        // Tells Hadoop which where to find compiled code and which logic classes to use
+        job.setJarByClass(Controller.class);
+        job.setMapperClass(DocumentSimilarityMapper.class);
+        job.setReducerClass(DocumentSimilarityReducer.class);
+
+        // Key-value output types; serving as the primary data structure for both input and output throughout the entire processing lifecycle
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+
+        // Input/output paths
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
